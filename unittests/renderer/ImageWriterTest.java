2449b48a380d043b6568339abf2d5a7f39a6148d
ester shmuel <eshmuel@g.jct.ac.il>
2022-04-18 10:51:22 +0300

    10:50

---
+++ unittests/renderer/ImageWriterTest.java
@@ -0,0 +1,13 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageWriterTest {
+
+    @Test
+    void writeToImage() {
+    }
+
+}
