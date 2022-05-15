7eb3b58094c4fb7d9659db1048f41fc586d669bd
ester shmuel <eshmuel@g.jct.ac.il>
2022-05-15 21:24:08 +0300

    21:23

--- src/geometries/Intersectable.java
+++ src/geometries/Intersectable.java
@@ -3,10 +3,6 @@ package geometries;
 import primitives.*;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 public abstract class Intersectable  {
 
     public List<Point> findIntersections(Ray ray) {
